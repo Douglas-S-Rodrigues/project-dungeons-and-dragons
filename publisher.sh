004db0f096c877482bbaa31d420fee94fcb2900f
Douglas Rodrigues <douglas.rodrigues.fw@gmail.com>
2022-08-16 18:31:54 -0300

    fix

---
+++ publisher.sh
@@ -0,0 +1,21 @@
+### GIT FILTER-REPO ###
+
+## NÃO EXECUTE ESSE SCRIPT DIRETAMENTE
+## Esse script foi feito para uso do
+## script 'publisher.sh' fornecido 
+## pela Trybe. 
+
+[[ $# == 1 ]] && \
+[[ $1 == "trybe-security-parameter" ]] && \
+git filter-repo \
+    --path .trybe \
+    --path .github \
+    --path .vscode \
+    --path trybe.yml \
+    --path trybe-filter-repo.sh \
+    --path README.md \
+    --path tests \
+    --path jest.config.js \
+    --path images \
+    --path jest.d.ts \
+    --invert-paths --force
